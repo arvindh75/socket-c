eae0da5fbcde72851db98f44a4d9862f0934a291
arvindh75 <iiith@arvindharun.com>
2020-11-07 15:33:58 +0530

    sync issues

--- client_dir/client2.c
+++ client_dir/client2.c
@@ -6,14 +6,15 @@
 #include <arpa/inet.h>
 #include <string.h>
 #include <fcntl.h>
+#include <time.h>
 
 #define PORT 8000
-
-#define BUFSIZE 100000
+#define BUFSIZE 131072
 
 void getf(int socketfd) {
     char *arg;
     char filename [BUFSIZE];
+    char ack[BUFSIZE];
     char size [BUFSIZE];
     char buff [BUFSIZE];
     arg = strtok(NULL, " \t");
@@ -22,70 +23,76 @@ void getf(int socketfd) {
         memset(&filename, '\0', sizeof(filename));
         strcpy(filename, arg);
         printf("\nFilename: %s\n", arg);
+        
+        //Sending filename
         if(send(socketfd, filename, BUFSIZE, 0) == -1) {
             perror("Sending filename");
             return;
         }
-
-        memset(&size, '\0', sizeof(size));
-        if(recv(socketfd, size, BUFSIZE, 0) == -1) {
-            perror("Reading from buffer");
+        memset(&ack, '\0', sizeof(ack));
+        
+        //Waiting for confirmation
+        if(recv(socketfd, ack, BUFSIZE, 0) == -1) {
+            perror("Receiving ack");
             return;
         }
+        printf("Ack for filename: [%s]\n", ack);
+        if(strcmp(ack, "ok")) {
+            printf("\nNot synchronised, Retry\n");
+            return;
+        }
+
+        //Reading file size
+        memset(&size, '\0', sizeof(size));
+        if(recv(socketfd, size, BUFSIZE, 0) == -1 || sizeof(size) == 0) {
+            perror("Reading from buffer");
+            memset(&ack, '\0', sizeof(ack));
+            strcpy(ack, "no");
+            printf("Sending no ack for filesize\n");
+            if(send(socketfd, ack, sizeof(ack), 0) == -1) {
+                perror("Sending ack");
+                return;
+            }
+            return;
+        }
+        printf("File size: [%s]\n", size);
+        
+        //Sending confirmation
+        memset(&ack, '\0', sizeof(ack));
+        strcpy(ack, "ok");
+        printf("Sending ok ack for filesize\n");
+        if(send(socketfd, ack, sizeof(ack), 0) == -1) {
+            perror("Sending ack");
+            return;
+        }
+        memset(&ack, '\0', sizeof(ack));
         if(!strcmp(size, "0")) {
             printf("File not found!\n");
             return;
         }
-        printf("File size: %d\n", atoi(size));
         
+        //Opening file
         FILE* fp2 = fopen(arg, "wb");
-        ssize_t n;
         int size_i = (int) atoi(size);
-        memset(&size, '\0', sizeof(size));
-        int written_lines = 1;
-        int rem = size_i % BUFSIZE;
-        int num_loops = (size_i/BUFSIZE);
+        //int written_lines = 1;
         float progress = 0.0;
         int read_ret = 0;
         memset(&buff, '\0', sizeof(buff));
         
-        if(rem != 0) {
-            if((read_ret = read(socketfd, buff, BUFSIZE)) == -1) {
-                perror("Reading from buffer");
-                break;
-            }
+        //Reading data
+        while((read_ret = read(socketfd, buff, BUFSIZE)) > 0) {
             if(fwrite(buff, sizeof(char), read_ret, fp2) < read_ret) {
                 perror("Writing content");
                 return;
             }
+            //printf("\nWriting a line - %d [%d]\n", written_lines, read_ret);
             progress += (float)(read_ret)/size_i;
             sprintf(prostr, "\rProgress : %.2f %c", progress * 100, '%');
             write(1, prostr, strlen(prostr));
-            printf("\nWriting a line in rem - %d\n", written_lines);
-            written_lines++;
+            //written_lines++;
             memset(&buff, '\0', sizeof(buff));
         }
         
-        printf("Num loops: %d\n", num_loops);
-        int rec_size;
-        int rec = 0;
-        int rec_total = 0;
-        while(num_loops--) {
-            if((read_ret = read(socketfd, buff, BUFSIZE)) == -1) {
-                perror("Reading from buffer");
-                break;
-            }
-            if(fwrite(buff, sizeof(char), read_ret, fp2) < read_ret) {
-                perror("Writing content");
-                return;
-            }
-            printf("\nWriting a line - %d [%d]\n", written_lines, read_ret);
-            progress += (float)(read_ret)/size_i;
-            sprintf(prostr, "\rProgress : %.2f %c", progress * 100, '%');
-            write(1, prostr, strlen(prostr));
-            written_lines++;
-            memset(&buff, '\0', sizeof(buff));
-        }
         printf("\nFinished writing the contents\n");
         fclose(fp2);
         arg = strtok(NULL, " \t");
@@ -120,7 +127,7 @@ int main(int argc, char const *argv[]) {
     while(1) {
         leninp = 0;
         exit_read = 0;
-        printf("client> ");
+        printf("\nclient> ");
         while (exit_read == 0) {
             c = getchar();
             if (c == EOF) {
--- server_dir/server2.c
+++ server_dir/server2.c
@@ -5,79 +5,102 @@
 #include <netinet/in.h>
 #include <string.h>
 #include <fcntl.h>
+#include <time.h>
 
 #define PORT 8000
-#define BUFSIZE 100000
+
+#define BUFSIZE 131072
 
 void sendf(int socketfd) {
-    ssize_t n;
     char filename[BUFSIZE];
+    char ack[BUFSIZE];
     char buff [BUFSIZE];
     char line [BUFSIZE];
     int it=0;
     memset(&buff, '\0', sizeof(buff));
     memset(&filename, '\0', sizeof(filename));
+    memset(&ack, '\0', sizeof(ack));
+    
     //Reading file name
     if(read(socketfd, filename, BUFSIZE) == -1) {
-        perror("Reading from buffer");
+        perror("Reading file name");
+        memset(&ack, '\0', sizeof(ack));
+        strcpy(ack, "no");
+        if(send(socketfd, ack, sizeof(ack), 0) == -1) {
+            perror("Sending ack");
+            return;
+        }
+    }
+    
+    //Sending ack
+    memset(&ack, '\0', sizeof(ack));
+    strcpy(ack, "ok");
+    printf("Sending ok ack for filename\n");
+    if(send(socketfd, ack, sizeof(ack), 0) == -1) {
+        perror("Sending ack");
+        return;
     }
     printf("Finished getting the name - [%s]\n", filename);
+    
+    //Opening files
     int fp = open(filename, O_RDONLY);
     FILE* fp2 = fopen(filename, "r");
-    if(fp == -1) {
+    if(fp2 == NULL) {
         perror("File");
-        if(send(socketfd, "0", 1, 0) == -1) {
-            perror("Sending size");
+        memset(&ack, '\0', sizeof(ack));
+        strcpy(ack, "0");
+        printf("File not found\n");
+        if(send(socketfd, ack, sizeof(ack), 0) == -1) {
+            perror("Sending 0 size");
+            return;
         }
         return;
     }
+    
     //Getting file size
     int fsize = lseek(fp, 0, SEEK_END);
+    close(fp);
     int length = snprintf(NULL, 0,"%d",fsize);
     char fsize_st[length+1];
     sprintf(fsize_st, "%d", fsize);
-    printf("Size: %d\n", fsize);
-    if(send(socketfd, fsize_st, BUFSIZE, 0) == -1) {
+    memset(&buff, '\0', sizeof(buff));
+    strcpy(buff, fsize_st);
+    printf("Size: %s\n", buff);
+    
+    //Sending file size
+    if(send(socketfd, buff, sizeof(buff), 0) == -1) {
         perror("Sending size");
         return;
     }
-    lseek(fp, 0, SEEK_SET);
-    //Sending file
-    printf("Sending file\n");
-    int num_lines=1;
-    int read_size;
-    int rem = fsize % BUFSIZE;
-    int num_loops = (fsize/BUFSIZE);
-    memset(&line, '\0', sizeof(line));
-    if(rem != 0) {
-        if((read_size = fread(line, sizeof(char), rem, fp2)) < 0) {
-            printf("Reading file error\n");
-            return;
-        }
-        if(send(socketfd, line, read_size, 0) == -1) {
-            perror("Sending line");
-            return;
-        }
-        printf("Sent a line from rem - %d\n", num_lines);
-        num_lines++;
+    memset(&ack, '\0', sizeof(ack));
+    
+    //Waiting for ack
+    if(recv(socketfd, ack, BUFSIZE, 0) == -1) {
+        perror("Receiving ack");
+        return;
+    }
+    printf("Ack for filesize: [%s]\n", ack);
+    if(strcmp(ack, "ok")) {
+        printf("\nNot synchronised, Retry\n");
+        return;
     }
     
+    //Sending file
+    printf("Sending file\n");
+    //int num_lines=1;
+    int read_size;
     memset(&line, '\0', sizeof(line));
-    printf("Num loops: %d\n", num_loops);
     int sent_size;
-    while(num_loops--) {
-        if((read_size = fread(line, sizeof(char), BUFSIZE, fp2)) < 0) {
-            printf("Reading file error\n");
-            return;
-        }
+    while ((read_size = fread(line, sizeof(char), BUFSIZE, fp2)) > 0) {
         if((sent_size = send(socketfd, line, read_size, 0)) == -1) {
             perror("Sending line");
             return;
         }
-        printf("Sent a line - %d [%d]\n", num_lines, sent_size);
-        num_lines++;
+        //printf("Sent a line - %d [%d]\n", num_lines, sent_size);
+        //num_lines++;
         memset(&line, '\0', sizeof(line));
     }
+    fclose(fp2);
     printf("Sent file \n");
 }
 
@@ -86,9 +109,6 @@ int main(int argc, char const *argv[]) {
     struct sockaddr_in address;
     int opt = 1;
     int addrlen = sizeof(address);
-    char buffer[1024] = {0};
-    char *hello = "Hello from server";
-
     if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
         perror("Socket file descriptor");
         exit(EXIT_FAILURE);
@@ -112,7 +132,9 @@ int main(int argc, char const *argv[]) {
         perror("accept");
         exit(EXIT_FAILURE);
     }
-    while(1)
+    while(1) {
         sendf(new_socket);
+        sleep(5);
+    }
     return 0;
 }
